4b5d92416d9c2968dadd0d9774ae79ab08763b8a
nigel <nigel@hardlee.com>
2022-01-25 21:41:14 -0800

    Can also send MQTT, simple proof of concept works in both directions

--- mqttapp/consumers.py
+++ mqttapp/consumers.py
@@ -2,6 +2,7 @@ import datetime
 from asgiref.sync import async_to_sync
 from channels.consumer import SyncConsumer
 from channels.layers import get_channel_layer
+import paho.mqtt.client as paho
 
 
 class MqttConsumer(SyncConsumer):
@@ -18,10 +19,19 @@ class MqttConsumer(SyncConsumer):
                 'message': {"rf_outlet_toggle": 1}
             }
         )
+        # Temp, just to prove you can send to MQTT as well!
+        async_to_sync(channel_layer.send)('mqtt.pub', {  # also needs to be mqtt.pub
+            'type': 'mqtt.pub',  # necessary to be mqtt.pub
+            'text': {
+                'topic': 'testmq',
+                'payload': "bigdawg"
+            }
+        })
         print("sub topic: {0}, payload: {1}".format(topic, payload))
 
     def mqtt_pub(self, event):
         topic = event['text']['topic']
         payload = event['text']['payload']
         # do something with topic and payload
+
         print("pub topic: {0}, payload: {1}".format(topic, payload))
