f2de3a1c6c8961bf80a229c23626587777ed180e
nigel <nigel@hardlee.com>
2022-01-29 11:43:40 -0800

    Sends mqtt to arduino, and arduino responds with status properly

--- devices/consumers.py
+++ devices/consumers.py
@@ -5,20 +5,20 @@ from asgiref.sync import async_to_sync
 from channels.consumer import SyncConsumer
 from channels.generic.websocket import WebsocketConsumer
 from django.core.exceptions import ObjectDoesNotExist
-from devices.models import RFOutlet
+from devices.models import RFOutlet, RGBLight
 
 
 class DevicesConsumer(WebsocketConsumer):
     def connect(self):
         async_to_sync(self.channel_layer.group_add)(
-            "testmqtt",
+            "device_updates",
             self.channel_name
         )
         self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
-            "testmqtt",
+            "device_updates",
             self.channel_name
         )
         pass
@@ -27,24 +27,25 @@ class DevicesConsumer(WebsocketConsumer):
         print(text_data)
         try:
             text_data_json = json.loads(text_data)
-            message = text_data_json['rf_outlet_toggle']
-            outlet = RFOutlet.objects.get(id=int(message))
-            outlet.toggle()
-            self.send(text_data=outlet.get_json_state())
+            if 'rf_outlet_toggle' in text_data_json:
+                message = text_data_json['rf_outlet_toggle']
+                outlet = RFOutlet.objects.get(id=int(message))
+                outlet.toggle()
+                self.send(text_data=outlet.get_json_state())
+            elif 'rgb_light_toggle' in text_data_json:
+                message = text_data_json['rgb_light_toggle']
+                light = RGBLight.objects.get(id=int(message))
+                light.toggle()
         except ObjectDoesNotExist as e:
+            print("OBJECT DOESNT EXIST")
             # print(e.what())
             return
         except TypeError as e:
             return
 
-    def handle_mqtt(self, text_data, **kwargs):
-        print("handle mqtt")
+    def mqtt_rgb_light_update(self, text_data, **kwargs):
         try:
-            message = text_data['message']["rf_outlet_toggle"]
-            print(message)
-            outlet = RFOutlet.objects.get(id=int(message))
-            outlet.toggle()
-            self.send(text_data=outlet.get_json_state())
+            self.send(text_data=text_data['message'])
         except ObjectDoesNotExist as e:
             # print(e.what())
             return
--- devices/models.py
+++ devices/models.py
@@ -1,6 +1,54 @@
+from asgiref.sync import async_to_sync
 from django.db import models
 import socket
 import json
+
+from channels.layers import get_channel_layer
+
+
+class RGBLight(models.Model):
+    name = models.CharField(max_length=300)
+    unique_id = models.IntegerField(unique=True)
+    is_on = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    red = models.IntegerField(default=0)
+    green = models.IntegerField(default=0)
+    blue = models.IntegerField(default=0)
+    # static
+    rgb_topic = "rgb-light"
+
+    def toggle(self):
+        print("toggle")
+        print(self.is_on)
+        self.is_on = not self.is_on
+        print(self.is_on)
+        self.set_color_mqtt()
+
+    def get_state_dict(self):
+        return {"id": self.id, "red": self.red, "green": self.green, "blue": self.blue, "is_on": self.is_on,
+                "is_active": self.is_active}
+
+    def get_json_state(self):
+        return json.dumps({
+            'rgb_light_status': self.get_state_dict()
+        })
+
+    def set_color_mqtt(self):
+        topic = self.rgb_topic + "/" + str(self.unique_id) + "/set-color"
+        channel_layer = get_channel_layer()
+        print("topic sending " + topic)
+        print(self.is_on)
+        status = {"id": self.id, "red": self.red, "green": self.green, "blue": self.blue, "is_on": self.is_on,
+                  "is_active": self.is_active}
+        print(status)
+        async_to_sync(channel_layer.send)('mqtt.pub', {  # also needs to be mqtt.pub
+            'type': 'mqtt.pub',  # necessary to be mqtt.pub
+            'text': {
+                'topic': topic,
+                'payload': json.dumps(status)
+            }
+        })
+        pass
 
 
 class RF433Module(models.Model):
