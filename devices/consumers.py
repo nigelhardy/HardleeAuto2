723dc1dddce431c7d6a4d1f7058fc8c3484a6491
nigel <nigel@hardlee.com>
2022-03-24 13:44:01 -0700

    added basic security/auth requirements

--- devices/consumers.py
+++ devices/consumers.py
@@ -11,11 +11,13 @@ from channels.layers import get_channel_layer
 
 class DevicesConsumer(WebsocketConsumer):
     def connect(self):
-        async_to_sync(self.channel_layer.group_add)(
-            "device_updates",
-            self.channel_name
-        )
-        self.accept()
+        username = self.scope["user"]
+        if username.is_authenticated:
+            async_to_sync(self.channel_layer.group_add)(
+                "device_updates",
+                self.channel_name
+            )
+            self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
@@ -25,34 +27,36 @@ class DevicesConsumer(WebsocketConsumer):
         pass
 
     def receive(self, text_data, **kwargs):
-        print(text_data)
-        try:
-            text_data_json = json.loads(text_data)
-            if 'rf_outlet_toggle' in text_data_json:
-                message = text_data_json['rf_outlet_toggle']
-                outlet = RF433Outlet.objects.get(id=int(message))
-                outlet.toggle()
-                self.send(text_data=outlet.get_json_state())
-            elif 'rgb_light_toggle' in text_data_json:
-                message = text_data_json['rgb_light_toggle']
-                light = RGBLight.objects.get(id=int(message))
-                light.toggle()
-            elif 'activate_garage_door':
-                topic = "relay/101/garage-door"
-                channel_layer = get_channel_layer()
-                async_to_sync(channel_layer.send)('mqtt.pub', {  # also needs to be mqtt.pub
-                    'type': 'mqtt.pub',  # necessary to be mqtt.pub
-                    'text': {
-                        'topic': topic,
-                        'payload': json.dumps({})
-                    }
-                })
-        except ObjectDoesNotExist as e:
-            print("OBJECT DOESNT EXIST")
-            # print(e.what())
-            return
-        except TypeError as e:
-            return
+        username = self.scope["user"]
+        if username.is_authenticated:
+            print(text_data)
+            try:
+                text_data_json = json.loads(text_data)
+                if 'rf_outlet_toggle' in text_data_json:
+                    message = text_data_json['rf_outlet_toggle']
+                    outlet = RF433Outlet.objects.get(id=int(message))
+                    outlet.toggle()
+                    self.send(text_data=outlet.get_json_state())
+                elif 'rgb_light_toggle' in text_data_json:
+                    message = text_data_json['rgb_light_toggle']
+                    light = RGBLight.objects.get(id=int(message))
+                    light.toggle()
+                elif 'activate_garage_door':
+                    topic = "relay/101/garage-door"
+                    channel_layer = get_channel_layer()
+                    async_to_sync(channel_layer.send)('mqtt.pub', {  # also needs to be mqtt.pub
+                        'type': 'mqtt.pub',  # necessary to be mqtt.pub
+                        'text': {
+                            'topic': topic,
+                            'payload': json.dumps({})
+                        }
+                    })
+            except ObjectDoesNotExist as e:
+                print("OBJECT DOESNT EXIST")
+                # print(e.what())
+                return
+            except TypeError as e:
+                return
 
     def mqtt_rgb_light_update(self, text_data, **kwargs):
         try:
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from devices.models import RF433Outlet, RGBLight
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="admin/")
 def index(request):
     rf_outlets = RF433Outlet.objects.all()
     rgb_lights = RGBLight.objects.all()
