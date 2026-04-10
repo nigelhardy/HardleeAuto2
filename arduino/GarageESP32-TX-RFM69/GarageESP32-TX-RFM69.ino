7a066af0d6ef79bb54b7da686e4f1e5708a1991a
Nigel Hardy <nigeljames.12@gmail.com>
2026-04-10 15:04:05 -0700

    Updated to use credentials for feather/lora encryption key

--- arduino/GarageESP32-TX-RFM69/GarageESP32-TX-RFM69.ino
+++ arduino/GarageESP32-TX-RFM69/GarageESP32-TX-RFM69.ino
@@ -103,10 +103,7 @@ void setup()
   rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
 
   // The encryption key has to be the same as the one in the server
-  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                    0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10};
-
-  rf69.setEncryptionKey(key);
+  rf69.setEncryptionKey(LORA_ENCRYPTION_KEY);
   
   pinMode(LED, OUTPUT);
 
--- arduino/credentials.h
+++ arduino/credentials.h
@@ -1,9 +1,16 @@
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
+#include <stdint.h>
+
 // Copy to your project folder and fill in your credentials
 // See arduino/README.md for complete setup instructions
 const char* MQTT_USERNAME = "your_username";
 const char* MQTT_PASSWORD = "your_password";
+
+// LoRa RFM69 encryption key (16 bytes)
+// Used by GarageESP32-TX-RFM69 and Garage_32u4_Feather_RX
+uint8_t LORA_ENCRYPTION_KEY[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                                      0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
 
 #endif
