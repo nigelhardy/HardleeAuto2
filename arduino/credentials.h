2115a6605d76593e1f711a8a178cc5c297e5cfb2
Nigel Hardy <nigeljames.12@gmail.com>
2026-04-10 14:02:00 -0700

    Add readme and reference it in credentials

--- arduino/credentials.h
+++ arduino/credentials.h
@@ -1,9 +1,8 @@
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
-// Copy this file to your project folder and fill in your actual credentials
-// DO NOT commit credentials.h files with real values to git
-// Look at pass manager: https://mqtt-hardlee-rpi-not_a_real_ip
+// Copy to your project folder and fill in your credentials
+// See arduino/README.md for complete setup instructions
 const char* MQTT_USERNAME = "your_username";
 const char* MQTT_PASSWORD = "your_password";
 
